dc9e73f81120d077b32735f154c3e66190848101
Emanuell Faustino <emanuellfaustino@gmail.com>
2014-02-13 19:40:45 -0300

    @author tag

--- src/main/java/br/com/fip/gati/revistaonline/repositorio/Repositorio.java
+++ src/main/java/br/com/fip/gati/revistaonline/repositorio/Repositorio.java
@@ -2,6 +2,9 @@ package br.com.fip.gati.revistaonline.repositorio;
 
 import java.util.List;
 
+/**
+ * @author Marcos
+ */
 public interface Repositorio<T> {
 	public void save(T entity);
 	public void update(T entity);
